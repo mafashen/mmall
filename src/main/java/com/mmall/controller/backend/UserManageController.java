a719d7b2814a21c01b5b8d52d32cb986e54b8e52
mafashen <mafashen@51xianqu.net>
2018-05-07 15:00:02 +0800

    admin login

--- src/main/java/com/mmall/controller/backend/UserManageController.java
+++ src/main/java/com/mmall/controller/backend/UserManageController.java
@@ -5,6 +5,9 @@ import com.mmall.common.RoleEnum;
 import com.mmall.common.ServerResponse;
 import com.mmall.domain.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.SessionUtil;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,22 +21,25 @@ public class UserManageController {
 
 	@Autowired
 	private IUserService iUserService;
+	@Autowired
+	private SessionUtil sessionUtil;
 
 	@RequestMapping(value="login.do",method = RequestMethod.POST)
 	@ResponseBody
-	public ServerResponse<User> login(String username, String password, HttpSession session){
-		ServerResponse<User> response = iUserService.login(username,password);
-		if(response.isSuccess()){
-			User user = response.getData();
+	public ServerResponse<User> login(String username, String password, HttpSession session , HttpServletResponse response){
+		ServerResponse<User> ret = iUserService.login(username,password);
+		if(ret.isSuccess()){
+			User user = ret.getData();
 			if(user.getRole() == RoleEnum.ADMIN.getCode()){
 				//说明登录的是管理员
-				session.setAttribute(Const.CURRENT_USER,user);
-				return response;
+//				session.setAttribute(Const.CURRENT_USER,user);
+				sessionUtil.login(response , session , ret.getData());
+				return ret;
 			}else{
 				return ServerResponse.Failure("不是管理员,无法登录");
 			}
 		}
-		return response;
+		return ret;
 	}
 
 }
